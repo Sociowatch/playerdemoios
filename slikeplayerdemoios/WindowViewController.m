62ab30529affd3e5c04fef780adf4c12548299d9
Aravind kumar <aravind.kumar@timesinternet.in>
2017-12-21 12:31:24 +0530

    0.6.3 pod test

--- slikeplayerdemoios/WindowViewController.m
+++ slikeplayerdemoios/WindowViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    SlikeConfig *slikeConfig = [[SlikeConfig alloc] initWithTitle:@"NBT Khabar express 26 09 2016 new" withID:@"1ytcef9gl6" withSection:@"/videos/news" withMSId:@"56087249" posterImage:@"http://slike.indiatimes.com/thumbs/1x/11/1x115ai9g6/thumb.jpg"];
+    SlikeConfig *slikeConfig = [[SlikeConfig alloc] initWithTitle:@"NBT Khabar express 26 09 2016 new" withID:@"1x1ch55glk" withSection:@"/videos/news" withMSId:@"56087249" posterImage:@"http://slike.indiatimes.com/thumbs/1x/11/1x115ai9g6/thumb.jpg"];
     //    SlikeConfig *slikeConfig = [[SlikeConfig alloc] initWithTitle:@"NBT Khabar express 26 09 2016 new" withID:@"1_oprrpt0x" withSection:@"/videos/news" withMSId:@"56087249" posterImage:@"http://slike.indiatimes.com/thumbs/1x/11/1x115ai9g6/thumb.jpg"];
 
     slikeConfig.ssoid = @"7ccgp8cpng4vcw9rg2tqvlkqc";
@@ -31,6 +31,7 @@
     [slikeConfig setLatitudeLongitude:@"26.539345" Longitude:@"80.487820"];
     [slikeConfig setCountry_State_City:@"IN" State:@"UP" City:@"Unnao"];
     [slikeConfig setUserInformation:@"Male" Age:28];
+    slikeConfig.isSkipAds =  YES;
     //
     [[SlikePlayer getInstance] playVideo:slikeConfig inParent:self.viewPlayer withAds:nil withProgressHandler:^(SlikeEventType type, SlikePlayerState name, StatusInfo *statusInfo) {
         if(statusInfo != nil)
